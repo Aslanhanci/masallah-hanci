6d4385c9d34bd6a15cbfd6c17b6af306740aee2d
Aslanhanci <151629955+Aslanhanci@users.noreply.github.com>
2026-05-24 18:33:27 +0300

    Add files via upload

---
+++ Kalıtım Ve Arayüzler/Kalıtım Ve Arayüzler modul5_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace modul5_1
+{    class Insan
+    {
+        public double Boy, Kilo;
+        public string Ad;
+
+        public Insan(string ad)
+        {
+            this.Ad = ad;
+            this.Boy = 0.45;
+            this.Kilo = 3.5;
+        }
+
+        public void Buyu()
+        {
+            Boy += 0.5;
+            Kilo += 0.5;
+        }
+    }
+    class Ogrenci : Insan
+    {
+        public Ogrenci(string ad) : base(ad) { }
+        new public void Buyu()
+        {
+            this.Boy += 1.5;
+            this.Kilo += 1.5;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci birOgrenci = new Ogrenci("Masallah Hancı");
+
+            Console.WriteLine("Öğrencimiz {0}'un Boyu {1}, kilosu ise {2}", birOgrenci.Ad, birOgrenci.Boy, birOgrenci.Kilo);
+            birOgrenci.Buyu();
+
+            Console.WriteLine("Büyüdükten sonra Boyu: {0}, kilosu: {1}", birOgrenci.Boy, birOgrenci.Kilo);
+
+            Console.Read();
+        }
+    }
+}
---
+++ giriş/Giriş modul1_1/Giriş modul1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+namespace modul1_1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" hoş geldin!");
+
+            Console.Write("Lütfen adınızı girin: ");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("Memnun oldum " + ad);
+
+            Console.Read();
+        }
+    }
+}
